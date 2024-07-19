7b399071153c2dcadb81601a3be9b2a14f921c2a
RAQKID <officialraqkid@gmail.com>
2024-07-19 15:45:43 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -13,7 +13,7 @@ module.exports = {
         main: 0x2f3136,
     },
     keepAlive: parseBoolean(process.env.KEEP_ALIVE) || true, // for https://replit.com keep alive bot 24/7
-    searchEngine: process.env.SEARCH_ENGINE || "ytsearch, scsearch, ytmsearch", // ytsearch = youtube, scsearch = soundcloud, spsearch = spotify,
+    searchEngine: process.env.SEARCH_ENGINE || "ytmsearch", // ytsearch = youtube, scsearch = soundcloud, spsearch = spotify,
     maxPlaylistSize: parseInt(process.env.MAX_PLAYLIST_SIZE) || 100,
     botStatus: process.env.BOT_STATUS || 'online', // online, idle, dnd, invisible
     botActivity: process.env.BOT_ACTIVITY || 'WaveMusic', // set the bot activity
