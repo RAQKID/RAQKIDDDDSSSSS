45d2739979ed6c40c30a2eb48f52ee2b34931841
RAQKID <officialraqkid@gmail.com>
2024-07-21 17:13:15 +0800

    Update Distorsion.js

--- src/commands/Filters/Distorsion.js
+++ src/commands/Filters/Distorsion.js
@@ -24,7 +24,7 @@ module.exports = class Distorsion extends Command {
         client: ["SendMessages", "ViewChannel", "EmbedLinks"],
         user: ["ManageGuild"],
       },
-      slashCommand: true,
+      slashCommand: false,
       options: [],
     });
   }
